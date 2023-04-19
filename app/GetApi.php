43cdd0aa03128171713475a707da4e5f256033eb
rncs92 <123461096+rncs92@users.noreply.github.com>
2023-04-19 09:35:12 +0300

    pd

--- app/GetApi.php
+++ app/GetApi.php
@@ -1,25 +1,42 @@
 <?php
 
 namespace app;
+
 use GuzzleHttp\Client;
 
-class ApiClient
+class GetApi
 {
     private Client $client;
 
+    private array $currencies;
     public function __construct()
     {
         $this->client = new Client();
     }
 
-    public function getData()
+    public function getValues(): array
     {
         $url = 'https://www.latvijasbanka.lv/vk/ecb.xml';
         $response = $this->client->request('GET', $url);
         $result = simplexml_load_string($response->getBody()->getContents());
 
         foreach ($result->Currencies->Currency as $record) {
-            var_dump($record);
+            $this->currencies[] = new Currency(
+                (string)$record->ID,
+                (float)$record->Rate
+            );
+        }
+        return $this->currencies;
+    }
+
+    public function convert(float $amount, string $toCurrency)
+    {
+        $this->getValues();
+        /** @var Currency $currency */
+        foreach($this->currencies as $currency) {
+            if($toCurrency == $currency->getId()) {
+                return $amount * $currency->getRate();
+            }
         }
     }
 }
--- main.php
+++ main.php
@@ -1,4 +1,16 @@
 <?php
 
 require 'vendor/autoload.php';
+require 'app/GetApi.php';
+require 'app/Currency.php';
 
+use app\GetApi;
+
+$client = new GetApi();
+
+$amount = (float)readline('Enter the sum you want to convert: ');
+$currency = readline('Enter the currency ID you want to convert TO: ');
+
+$result = $client->convert($amount, $currency);
+var_dump($result);
+echo "You just converted $amount of Euros to $result $currency" . PHP_EOL;
